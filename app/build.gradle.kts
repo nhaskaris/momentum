e1fb9c41909b062745059a4ec196ce27a0a98679
Nick Haskaris <nickhaskaris@gmail.com>
2026-08-10 21:24:30 +0300

    Bump: Bump versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.eliteonetube.momentum"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.0.1-10"
+        versionCode = 13
+        versionName = "1.0.1-11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
